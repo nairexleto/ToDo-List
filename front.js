37f6b2de0e2653f238e37bb34ae472650780b199
nassim <129227654+nairexleto@users.noreply.github.com>
2023-11-04 10:44:57 +0100

    Add files via upload

--- front.js
+++ front.js
@@ -1,12 +1,24 @@
-const myadd = document.getElementById("addbtn");
+/*function add() {
+  const myList = document.querySelector("#task");
+  const listItem = document.createElement("li");
+  let myadd = document.getElementById("input");
+  listItem.textContent = myadd.value;
+  myList.append(listItem);
+}*/
+const btn = document.getElementById("addbtn");
 const myinput = document.getElementById("input");
 
-myadd.onclick = function () {
-    if (myinput.value == "") {
-        alert("u must type somthing");
-    }
-    else {
-        document.getElementById("task").innerHTML = myinput.value;
-        myinput.value = "";
-    }
-};
+function add() {
+  if (myinput.value === "") {
+    alert("u must type somthing");
+  } else {
+    /*console.log(myinput.value);*/
+
+    const myList = document.querySelector("#task");
+    const listItem = document.createElement("li");
+    let myadd = document.getElementById("input");
+    listItem.textContent = myadd.value;
+    myList.append(listItem);
+  }
+  myinput.value = "";
+}

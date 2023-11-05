b0354917368795002af7c1f7bcf908bfc3d6c0c4
nassim <129227654+nairexleto@users.noreply.github.com>
2023-11-05 22:49:10 +0100

    Update front.js

--- front.js
+++ front.js
@@ -1,13 +1,5 @@
-/*function add() {
-  const myList = document.querySelector("#task");
-  const listItem = document.createElement("li");
-  let myadd = document.getElementById("input");
-  listItem.textContent = myadd.value;
-  myList.append(listItem);
-}*/
 const btn = document.getElementById("addbtn");
 const myinput = document.getElementById("input");
-
 function add() {
   if (myinput.value === "") {
     alert("u must type somthing");
@@ -21,4 +13,25 @@ function add() {
     myList.append(listItem);
   }
   myinput.value = "";
+}
+const mylabbel = document.getElementById("time");
+update();
+setInterval(update, 1000);
+function update() {
+  let date = new Date();
+  mylabbel.innerHTML = formatdate(date);
+  function formatdate(date) {
+    let hour = date.getHours();
+    let minute = date.getMinutes();
+    let sec = date.getSeconds();
+    let pmoram = hour >= 12 ? " PM " : " AM ";
+    hour = zero(hour);
+    minute = zero(minute);
+    sec = zero(sec);
+    return hour + ":" + minute + ":" + sec + pmoram;
+  }
+}
+function zero(time) {
+  time = time.toString();
+  return time.length < 2 ? "0" + time : time;
 }

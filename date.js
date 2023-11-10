af7c03a03919d257d043db80b74993ede746451c
labed nassim <labednassim8@gmail.com>
2023-11-10 01:32:10 +0100

    update
     new changes

---
+++ date.js
@@ -0,0 +1,17 @@
+function addate() {
+  const myinser = document.getElementById("inser");
+  if (myinser.value === "") {
+    alert("enter a date ");
+  } else {
+    const myliste = document.querySelector("#tasklist");
+    const dateItem = document.createElement("li");
+    dateItem.textContent = myinser.value;
+    myliste.appendChild(dateItem);
+    const lienURL = "todoliste.html";
+    const lienElement = document.createElement("a");
+    lienElement.href = lienURL;
+    lienElement.textContent = " add task relative to this date" + myinser.value;
+    myliste.appendChild(lienElement);
+  }
+  myinser.value = "";
+}
--- front.js
+++ front.js
@@ -1,25 +1,11 @@
-const btn = document.getElementById("addbtn");
-const myinput = document.getElementById("input");
-function add() {
-  if (myinput.value === "") {
-    alert("u must type somthing");
-  } else {
-    /*console.log(myinput.value);*/
-
-    const myList = document.querySelector("#task");
-    const listItem = document.createElement("li");
-    let myadd = document.getElementById("input");
-    listItem.textContent = myadd.value;
-    myList.append(listItem);
-  }
-  myinput.value = "";
-}
 const mylabbel = document.getElementById("time");
 update();
 setInterval(update, 1000);
+
 function update() {
   let date = new Date();
   mylabbel.innerHTML = formatdate(date);
+
   function formatdate(date) {
     let hour = date.getHours();
     let minute = date.getMinutes();
@@ -31,7 +17,44 @@ function update() {
     return hour + ":" + minute + ":" + sec + pmoram;
   }
 }
+
 function zero(time) {
   time = time.toString();
   return time.length < 2 ? "0" + time : time;
+}
+
+const btn = document.getElementById("addbtn");
+const myinput = document.getElementById("input");
+
+function add() {
+  if (myinput.value === "") {
+    alert("You must type something");
+  } else {
+    const myList = document.querySelector("#task");
+    const listItem = document.createElement("li");
+    listItem.textContent = myinput.value;
+
+    // Ajouter le bouton de suppression
+    const deleteTaskBtn = document.createElement("button");
+    deleteTaskBtn.textContent = "x";
+    deleteTaskBtn.addEventListener("click", function () {
+      myList.removeChild(listItem);
+    });
+
+    // Ajouter le bouton de validation
+    const validateTaskBtn = document.createElement("buttonv");
+    validateTaskBtn.textContent = "✔";
+    validateTaskBtn.addEventListener("click", function () {
+      listItem.classList.toggle("checked");
+    });
+
+    // Ajouter les boutons à l'élément de la liste
+    listItem.appendChild(deleteTaskBtn);
+    listItem.appendChild(validateTaskBtn);
+
+    // Ajouter l'élément de la liste à la liste
+    myList.appendChild(listItem);
+  }
+
+  myinput.value = "";
 }
